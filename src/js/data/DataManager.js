66e82839cf6e5a6551ee6e60f4c24b6cab12c2fa
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-13 14:35:49 -0500

    added datamanager functions, started card

--- src/js/data/DataManager.js
+++ src/js/data/DataManager.js
@@ -2,12 +2,49 @@
 const DataManager = Object.create(null, {
     login: {
         value: () => {
-            
+
         }
     },
     getArticles: {
         value: () => {
 
+        }
+    },
+    getEvent: {
+        value: () => {
+            return fetch("http://localhost:8088/events")
+            .then(res => res.json())
+        }
+    },
+    saveEvent: {
+        value: (event) => {
+            return fetch("http://localhost:8088/events", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(event)
+            })
+            .then(result => result.json())
+        }
+    },
+    removeEvent: {
+        value: (eventId) => {
+            return fetch(`http://localhost:8088/events/${eventId}`, {
+                method: "Delete"
+            }).then(result => result.json())
+        }
+    },
+    editEvent: {
+        value: (eventId, event) => {
+            return fetch(`http://localhost:8088/events/${eventId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(event)
+            })
+            .then(result => result.json())
         }
     }
 })
---
+++ src/js/events/eventCard.js
@@ -0,0 +1,10 @@
+let dataManager = require("./data/DataManager");
+
+function makeEventCard(object) {
+    document.querySelector("#event-container").innerHTML+=
+    `<div class="event-card" id="event--${object.id}">
+    <h2 id="title--${object.id}">${object.title}</h2>
+    <h4 id="date--${object.id}">${object.date}</h4>
+    <h4 id="location--${object.id}">${object.location}</h4>
+    `
+}
