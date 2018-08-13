4ce2acc7355fcf3484fd609353f80afc326731c5
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-13 15:37:15 -0500

    completed event card

--- src/js/events/eventCard.js
+++ src/js/events/eventCard.js
@@ -1,12 +1,23 @@
-let dataManager = require("./data/DataManager");
+const makeEventCard = Object.create(null, {
+    renderEventForm: {
+        value: (object) => {
+            document.querySelector("#event-form").innerHTML+=
+            `<button id="new-event--${object.id}">New Event</button>`
+        }
+    },
+    renderEventList: {
+        value: (object) => {
+            document.querySelector("#event-list").innerHTML+=
+            `
+            <div class="event-card" id="event-list--${object.id}">
+            <h2 id="title--${object.id}">${object.title}</h2>
+            <h4 id="date--${object.id}">${object.date}</h4>
+            <h4 id="location--${object.id}">${object.location}</h4>
+            <button id="edit-event--${object.id}">Edit</button>
+            </div>
+            `
+        }
+    }
+})
 
-function makeEventCard(object) {
-    document.querySelector("#event-container").innerHTML+=
-    `<div class="event-card" id="event-form--${object.id}"></div>
-    <div class="event-card" id="event-list--${object.id}">
-    <h2 id="title--${object.id}">${object.title}</h2>
-    <h4 id="date--${object.id}">${object.date}</h4>
-    <h4 id="location--${object.id}">${object.location}</h4>
-    </div>
-    `
-}
+module.exports = makeEventCard
